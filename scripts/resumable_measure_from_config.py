34ff61b5c6212f52b9c0b47bbf95b88390901acd
Matthew Hartley <mhartley@cantab.net>
2021-02-03 17:41:44 +0000

    Add new scripts

---
+++ scripts/resumable_measure_from_config.py
@@ -0,0 +1,73 @@
+import logging
+import pathlib
+
+import click
+import parse
+import pandas as pd
+
+from dtoolbioimage import ImageDataSet
+from dtoolbioimage.segment import Segmentation3D
+from mruntools.config import Config
+from mruntools.run import run_process
+from mruntools.spec import specs_from_fpath_metadata, get_all_specs
+#from stacktools.data import InitialsSMS
+from stacktools.data import SegmentationMeasureStack
+from stacktools.measure import measure_all_regions
+
+
+logger = logging.getLogger(__file__)
+
+
+def generate_candidate_item_specs(config):
+
+    return specs_from_fpath_metadata(
+        config.segmentation_dirpath,
+        config.parse_templates
+    )
+
+
+@click.command()
+@click.argument('config_fpath')
+def main(config_fpath):
+
+    logging.basicConfig(level=logging.INFO)
+
+    config = Config.from_fpath(config_fpath)
+
+    specs = generate_candidate_item_specs(config)
+
+    ids = ImageDataSet(config.ids_uri)
+    tuple_lookup = {
+        st[0]: st
+        for st in ids.all_possible_stack_tuples()
+    }
+
+    def annotate_spec(spec):
+        spec_template = "Image_{n}"
+        spec_key = spec.template_repr(spec_template)
+        spec.image_name, spec.series_name, spec.sidx = tuple_lookup[spec_key]
+        spec.fname = spec.template_repr("Image_{n}.{ext}.csv")
+
+        return spec
+
+    annotated_specs = [annotate_spec(spec) for spec in specs]
+    completed_specs = get_all_specs(config.working_dirpath)
+
+    remaining_specs = set(annotated_specs) - set(completed_specs)
+    logging.info(f"Completed {len(completed_specs)}, {len(remaining_specs)} left")
+
+    def measure_from_spec(spec):
+        sms = InitialsSMS.from_config_and_spec(config, spec)
+        measures = measure_all_regions(sms)
+        measures['root_number'] = spec.n
+
+        return measures
+
+    selected_specs = remaining_specs
+    all_measures = run_process(selected_specs, measure_from_spec, config)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/segment_to_ds.py
@@ -0,0 +1,88 @@
+import logging
+import pathlib
+from types import SimpleNamespace
+
+import click
+import dtoolcore
+
+from dtoolbioimage import ImageDataSet, zoom_to_match_scales
+from dtoolbioimage.segment import sitk_watershed_segmentation
+
+from stacktools.config import Config
+
+
+logger  = logging.getLogger("initials")
+
+
+def process_image_and_series(config, spec, output_ds):
+
+    imageds = ImageDataSet(config.ids_uri)
+    wall_stack = imageds.get_stack(
+        spec.image_name,
+        spec.series_name,
+        0,
+        config.wall_channel
+    )
+    logger.info(f"Loaded {spec.series_name} with shape {wall_stack.shape}")
+    zoomed_wall_stack = zoom_to_match_scales(wall_stack)
+    snfmt = spec.series_name.replace(" ", "_").replace("#", "")
+
+    level = config.params['level']
+    logging.info(f"Segmenting with level {level}")
+    segmentation = sitk_watershed_segmentation(
+        zoomed_wall_stack,
+        level=config.params["level"]
+    )
+
+    wall_output_relpath = f"{spec.image_name}_{snfmt}_cell_wall.tif"
+    seg_output_relpath = f"{spec.image_name}_{snfmt}_segmentation_L{level}.tif"
+
+    wall_output_abspath = output_ds.prepare_staging_abspath_promise(wall_output_relpath)
+    seg_output_abspath = output_ds.prepare_staging_abspath_promise(seg_output_relpath)
+
+    segmentation.save(seg_output_abspath)
+    zoomed_wall_stack.save(wall_output_abspath)
+
+
+def stack_tuple_to_spec(stack_tuple):
+    names = ["image_name", "series_name", "sidx"]
+    tdict = dict(zip(names, stack_tuple))
+
+    return SimpleNamespace(**tdict)
+
+
+@click.command()
+@click.argument('config_fpath')
+def main(config_fpath):
+
+    logging.basicConfig(level=logging.INFO)
+
+    config = Config(config_fpath)
+
+    ids = ImageDataSet(config.ids_uri)
+
+    specs = [
+        stack_tuple_to_spec(tp)
+        for tp in ids.all_possible_stack_tuples()
+    ]
+
+
+    with dtoolcore.DataSetCreator(
+        config.output_name,
+        config.output_base_uri
+    ) as output_ds:
+        logging.info(f"Monitor at: {output_ds.staging_directory}")
+        for spec in specs:
+            try:
+                logging.info(f"Processing {spec}")
+                process_image_and_series(config, spec, output_ds)
+            except KeyError:
+                logging.error(f"Failed on {spec}")
+                
+        readme_str = config.as_readme_format()
+        output_ds.put_readme(readme_str)
+
+
+
+if __name__ == "__main__":
+    main()

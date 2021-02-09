d94646d4d7b54d317987406df170050264467a91
Matthew Hartley <mhartley@cantab.net>
2021-02-09 10:36:44 +0000

    Add visualisation script

---
+++ scripts/generate_spherefit_visualisations.py
@@ -0,0 +1,93 @@
+import os
+import click
+import logging
+import pathlib
+
+import numpy as np
+import pandas as pd
+
+from dtoolbioimage import ImageDataSet, Image as dbiImage
+from mruntools.config import Config
+from mruntools.spec import specs_from_fpath_metadata
+from stacktools.data import InitialsSMS, FCARootData
+from stacktools.vis import create_annotated_file_projection
+
+
+logger = logging.getLogger(__file__)
+
+
+def generate_candidate_item_specs(config):
+
+    return specs_from_fpath_metadata(
+        config.segmentation_dirpath,
+        config.parse_templates
+    )
+
+
+def annotated_specs_from_config(config):
+
+    specs = generate_candidate_item_specs(config)
+
+    ids = ImageDataSet(config.ids_uri)
+    tuple_lookup = {
+        st[0]: st
+        for st in ids.all_possible_stack_tuples()
+    }
+
+    def annotate_spec(spec):
+        spec_template = config.segmentation_spec_template
+        spec_key = spec.template_repr(spec_template)
+        spec.image_name, spec.series_name, spec.sidx = tuple_lookup[spec_key]
+        spec.fname = spec.template_repr(config.segmentation_fname_template)
+        spec.regions_fname = spec.template_repr(config.regions_fname_template)
+
+        return spec
+
+    annotated_specs = [annotate_spec(spec) for spec in specs]
+
+    return annotated_specs
+
+
+@click.command()
+@click.argument('config_fpath')
+def main(config_fpath):
+
+    logging.basicConfig(level=logging.INFO)
+
+    config = Config.from_fpath(config_fpath)
+
+    annotated_specs = annotated_specs_from_config(config)
+    logger.info(f"Founds specs: {annotated_specs}")
+
+    vis_dirpath = pathlib.Path(config.visualisation_dirpath)
+    vis_dirpath.mkdir(exist_ok=True, parents=True)
+
+    for spec in annotated_specs:
+        logger.info(f"Generating image for {spec}")
+
+        sms = InitialsSMS.from_config_and_spec(config, spec)
+        root_data = pd.read_csv(config.output_fpath)
+        root_data["file_id"] = root_data["label"]
+
+        fcaroot = FCARootData(
+            spec.image_name,
+            sms.wall_stack,
+            sms.measure_stack,
+            sms.segmentation,
+            root_data
+        )
+
+        sections = [
+            create_annotated_file_projection(fcaroot, fid, fcaroot.denoised_venus_stack)
+            for fid in fcaroot.files
+        ]
+
+        output_fpath = vis_dirpath/f"{spec.image_name}.png"
+        composite = np.vstack(sections).view(dbiImage)
+        composite.save(output_fpath)
+
+
+
+
+if __name__ == "__main__":
+    main()
